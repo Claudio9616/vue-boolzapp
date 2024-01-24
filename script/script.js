23f7249d60616a3205d6df920233348e95a31418
Claudio9616 <clares9611@gmail.com>
2024-01-24 19:53:10 +0100

    add milestone to fix

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
 console.log(Vue)
+
+const {createApp} = Vue
+const app = createApp({
+    name: 'Boolzap',
+    data(){
+        return {
+           data
+        }
+    },
+    methods: {
+       
+    }
+})
+app.mount('#root')
